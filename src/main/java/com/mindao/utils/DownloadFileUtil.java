1dcbe33f8f53a53d0b2ef2eabe98dc71034024b3
gxh <gongxianghua@hyperchain.cn>
2019-07-24 20:10:53 +0800

    MY_CRM

---
+++ src/main/java/com/mindao/utils/DownloadFileUtil.java
@@ -0,0 +1,60 @@
+package com.mindao.utils;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DownloadFileUtil {
+	public final static Logger logger = LoggerFactory.getLogger(DownloadFileUtil.class);
+	
+	
+	public static  void downloadFile(HttpServletResponse response,HttpServletRequest request,String fileToDownload,InputStream inputStream,int fileLength) throws Exception {
+
+		OutputStream os =null;
+		try {
+ 
+			logger.debug("fileName：" + fileToDownload);
+			String fileName = new String(fileToDownload.getBytes("GBK"), "ISO-8859-1");
+			logger.debug("fileName" + fileName);
+		
+			response.setContentType("file/plain");
+			
+			response.setHeader("Content-disposition", "attachment; filename="  
+                    +  fileName);  
+ 
+           
+	        response.reset();
+	        response.setContentType("application/octet-stream;charset=utf-8");
+	        response.setHeader("Content-Disposition", "attachment;filename=" +new String(fileToDownload.getBytes("gbk"),"iso8859-1") );
+	        response.setHeader("Content-Length", "" + fileLength);
+	        
+	        
+	        os = response.getOutputStream();  
+ 
+	        byte[] buffer = new byte[1024];
+			int r;
+			while ((r = inputStream.read(buffer)) > 0) {
+				os.write(buffer, 0, r);
+				os.flush();
+			}
+ 
+  
+		} catch (Exception e) {
+			//logger.error(e.getMessage(),e);
+			throw e;
+		}finally{
+			if (inputStream!=null){
+				inputStream.close();
+			}	
+			if (os != null) {
+				os.close();
+			}
+		}
+	}
+ 	
+}
